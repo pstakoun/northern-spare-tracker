7ff507b83fbe9a666781c70ae1561a6aef059ae5
Peter Stakoun <peter@stakoun.com>
2017-02-08 23:22:38 -0500

    Update student creation

--- studentio.cpp
+++ studentio.cpp
@@ -13,52 +13,61 @@ static inline std::string &rtrim(std::string &s) {
 
 std::vector<Student*> StudentIO::readStudents()
 {
-    std::string path = QCoreApplication::applicationDirPath().toStdString() + "/data/day1.csv";
+    std::string paths[] = {QCoreApplication::applicationDirPath().toStdString() + "/data/day1.csv",
+                           QCoreApplication::applicationDirPath().toStdString() + "/data/day2.csv"};
 
     std::vector<Student*> students;
 
-    std::ifstream fileIn;
-    fileIn.open(path);
+    for (int i = 0; i < paths.length; i++) {
+        std::string path = paths[i];
 
-    if (!fileIn.is_open())
-        return students;
+        std::ifstream fileIn;
+        fileIn.open(path);
 
-    std::string line;
+        if (!fileIn.is_open())
+            return students;
 
-    std::getline(fileIn, line);
+        std::string line;
 
-    std::vector<std::string> header;
-    std::istringstream ss(rtrim(line));
-    std::string token;
-    while(std::getline(ss, token, ',')) {
-        header.push_back(token);
-    }
+        std::getline(fileIn, line);
 
-    int idIndex = -1, fnameIndex = -1, lnameIndex = -1, periodIndex = -1;
-    for (int i = 0; i < header.size(); i++) {
-        if ("student_no" == header[i])
-            idIndex = i;
-        else if ("preferred_first_name" == header[i])
-            fnameIndex = i;
-        else if ("preferred_surname" == header[i])
-            lnameIndex = i;
-        else if ("school_period" == header[i])
-            periodIndex = i;
-    }
-
-    if (idIndex == -1 || fnameIndex == -1 || lnameIndex == -1 || periodIndex == -1)
-        return students;
-
-    std::vector<std::string> args;
-    while (std::getline(fileIn, line)) {
+        std::vector<std::string> header;
         std::istringstream ss(rtrim(line));
         std::string token;
         while(std::getline(ss, token, ',')) {
-            args.push_back(token);
+            header.push_back(token);
         }
-        // TODO find and modify student with ID if exists
-        students.push_back(new Student(QString::fromStdString(args[idIndex]), QString::fromStdString(args[fnameIndex]), QString::fromStdString(args[lnameIndex])));
-        args.clear();
+
+        int idIndex = -1, fnameIndex = -1, lnameIndex = -1, periodIndex = -1;
+        for (int i = 0; i < header.size(); i++) {
+            if ("student_no" == header[i])
+                idIndex = i;
+            else if ("preferred_first_name" == header[i])
+                fnameIndex = i;
+            else if ("preferred_surname" == header[i])
+                lnameIndex = i;
+            else if ("school_period" == header[i])
+                periodIndex = i;
+        }
+
+        if (idIndex == -1 || fnameIndex == -1 || lnameIndex == -1 || periodIndex == -1)
+            return students;
+
+        std::vector<std::string> args;
+        while (std::getline(fileIn, line)) {
+            std::istringstream ss(rtrim(line));
+            std::string token;
+            while(std::getline(ss, token, ',')) {
+                args.push_back(token);
+            }
+            // TODO find and modify student with ID if exists
+            Student newStudent = new Student(QString::fromStdString(args[idIndex]),
+                                             QString::fromStdString(args[fnameIndex]),
+                                             QString::fromStdString(args[lnameIndex]));
+            newStudent.addSpare(std::stoi(args[periodIndex]) + i * 4 - 1);
+            students.push_back(newStudent);
+            args.clear();
+        }
     }
 
     return students;
